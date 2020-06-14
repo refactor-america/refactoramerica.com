06bc351b5212e8f0770dc5a94f4959d41eb036b9
Refactor America <info@refactoramerica.com>
2020-06-13 18:15:28 -0700

    Init react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Black Lives Matter
+        </p>
+        <p>
+          Crowdsourced project to support Black-owned businesses.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/black lives matter/i);
+  expect(titleElement).toBeInTheDocument();
+});
