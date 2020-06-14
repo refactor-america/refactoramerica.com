4cb3abf66233cac3114970c8c9c65f88a7adfe4d
Amanda Flink <amanda.j.flink@gmail.com>
2020-06-13 17:58:01 -0700

    Updates

---
+++ precache-manifest.a17fc701249eefb02a986bd8b0bfdf64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0412c96591904ac38f1afa2c645b5e7f",
+    "url": "/refactoramerica.com/index.html"
+  },
+  {
+    "revision": "a88f18d4f127d281ce17",
+    "url": "/refactoramerica.com/static/css/main.713ad2e0.chunk.css"
+  },
+  {
+    "revision": "fc0399960dc2dcf98dfc",
+    "url": "/refactoramerica.com/static/js/2.8c358dc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/refactoramerica.com/static/js/2.8c358dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88f18d4f127d281ce17",
+    "url": "/refactoramerica.com/static/js/main.d7d1e7b3.chunk.js"
+  },
+  {
+    "revision": "0163fe7eb038fb67ed3a",
+    "url": "/refactoramerica.com/static/js/runtime-main.f9b25af1.js"
+  }
+]);
